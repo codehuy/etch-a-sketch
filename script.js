996ab2d8d7f0f3f8f62fe419ece2079dfb3c39af
Huy <64755394+codehuy@users.noreply.github.com>
2022-12-05 16:40:43 -0500

    added color picker function

--- script.js
+++ script.js
@@ -1,23 +1,91 @@
 
-const colorPicker = document.querySelector('#colorPicker');
-const colorBtn = document.querySelector('#clearBtn');
-const eraserBtn = document.querySelector('#eraserBtn');
-const clearBtn = document.querySelector('#clearBtn');
+const board = document.querySelector('.board');
 
-
-function createBoard(size){
-    let board = document.querySelector('.board');
-
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    let numDivs = size * size; 
-
-    for (i = 0; i < numDivs; i++){
-        let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow';
-        board.insertAdjacentElement('beforeend', div);
+function createBoard(){
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        board.appendChild(div); 
     }
-}
+};
 
-createBoard(32);
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+};
+
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    screenVal.textContent = `${val} x ${val}`;
+    removeAllChildNodes(board);
+    board.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        board.appendChild(div); 
+    }
+});
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function(){
+    let val = document.getElementById('slider').value;
+    let cell = board.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].style.backgroundColor = 'white';
+    }
+});
+
+function getRandomColor(){
+    let letters = '0123456789ABCDEF'
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color; 
+};
+
+const rgb = document.querySelector('#rgb');
+rgb.addEventListener('click', function(){
+    let val = document.getElementById('slider').value; 
+    let cell = board.children; 
+    for(let i = 0; i < val*val; i++){
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        })
+    }
+});
+
+const black = document.querySelector('#black');
+black.addEventListener('click', function(){
+    let val = document.getElementById('slider').value;
+    let cell = board.children;
+    for(let i = 0; i < val * val; i++){
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+    }
+});
+
+const chooseColor = document.querySelector('#color');
+chooseColor.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    let newColor = document.getElementById('color').value;
+    let cell = board.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = newColor;
+        })
+    }
+});
+
+
+createBoard();
